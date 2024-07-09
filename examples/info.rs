3915c0c2a2458c3e3bae7cce8623a8be0d20ef8f
Jasper Bekkers <jasper@traverseresearch.nl>
2024-07-09 21:15:17 +0200

    fmt

--- examples/info.rs
+++ examples/info.rs
@@ -2,5 +2,8 @@ use cpuinfo_rs::CpuInfo;
 
 fn main() {
     let info = CpuInfo::new();
-    let _ = std::fs::write("info.txt", serde_json::to_string(&info.processors()).unwrap());
+    let _ = std::fs::write(
+        "info.txt",
+        serde_json::to_string(&info.processors()).unwrap(),
+    );
 }
