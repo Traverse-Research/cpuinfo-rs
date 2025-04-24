b3c8f82bfb90aedc00e35f8a7de55abdc9c53670
Max de Danschutter <max.de.danschutter@traverseresearch.nl>
2025-04-24 14:32:34 +0200

    generate bindings ci

--- build.rs
+++ build.rs
@@ -80,13 +80,12 @@ const FREEBSD_X86_SRCS: &[&str] = &["src/x86/freebsd/init.c"];
 /// Targets for which bindings will be generated.
 const BINDGEN_SUPPORTED_TARGETS: &[&str] = &[
     "x86_64-unknown-linux-gnu",
-    // "x86_64-apple-darwin",
+    "x86_64-apple-darwin",
     "x86_64-pc-windows-msvc",
-    "x86_64-unknown-freebsd",
     "aarch64-linux-android",
     "aarch64-unknown-linux-gnu",
     "aarch64-pc-windows-msvc",
-    // "aarch64-apple-darwin",
+    "aarch64-apple-darwin",
     "armv7-unknown-linux-gnueabihf",
 ];
 
